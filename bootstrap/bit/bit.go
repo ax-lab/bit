2ed1cf38aa6a73933c16cd7b65e8126ad167056b
ax-ab <98651633+ax-lab@users.noreply.github.com>
2024-02-07 18:12:37 +1300

    refactor errors to a separate file

--- bootstrap/bit/bit.go
+++ bootstrap/bit/bit.go
@@ -1,10 +1,7 @@
 package bit
 
 import (
-	"cmp"
 	"context"
-	"fmt"
-	"sort"
 	"strings"
 	"sync"
 	"time"
@@ -12,29 +9,6 @@ import (
 	"axlab.dev/bit/files"
 	"axlab.dev/bit/logs"
 )
-
-type CompilerError struct {
-	Span    Span
-	Message string
-	Args    []any
-}
-
-func (err CompilerError) String() string {
-	msg := err.Message
-	if len(err.Args) > 0 {
-		msg = fmt.Sprintf(msg, err.Args...)
-	}
-	loc := fmt.Sprintf("%s:%s", err.Span.Source().Name(), err.Span.Location().String())
-	txt := err.Span.DisplayText(0)
-	if len(txt) > 0 {
-		txt = fmt.Sprintf("\n\n    | %s", txt)
-	}
-	return fmt.Sprintf("at %s: %s%s", loc, msg, txt)
-}
-
-func (err CompilerError) Error() string {
-	return err.String()
-}
 
 type Compiler struct {
 	ctx context.Context
@@ -181,22 +155,4 @@ func (program *Program) ClearBuild() {
 		logs.Out("\n>>> Removing `%s`\n", program.config.InputPath)
 		program.compiler.buildDir.RemoveAll(program.config.BuildPath)
 	}()
-}
-
-func SortErrors(errs []error) {
-	sort.Slice(errs, func(i, j int) bool {
-		errA := errs[i]
-		errB := errs[j]
-		cmpErrA, okA := errA.(CompilerError)
-		cmpErrB, okB := errB.(CompilerError)
-		if okA != okB {
-			return !okA // non-compilation errors first
-		}
-
-		if orderByMessage := !okA; orderByMessage {
-			return cmp.Compare(errA.Error(), errB.Error()) < 0
-		}
-
-		return cmpErrA.Span.Compare(cmpErrB.Span) < 0
-	})
 }
---
+++ bootstrap/bit/errors.go
@@ -0,0 +1,48 @@
+package bit
+
+import (
+	"cmp"
+	"fmt"
+	"sort"
+)
+
+type CompilerError struct {
+	Span    Span
+	Message string
+	Args    []any
+}
+
+func (err CompilerError) String() string {
+	msg := err.Message
+	if len(err.Args) > 0 {
+		msg = fmt.Sprintf(msg, err.Args...)
+	}
+	loc := fmt.Sprintf("%s:%s", err.Span.Source().Name(), err.Span.Location().String())
+	txt := err.Span.DisplayText(0)
+	if len(txt) > 0 {
+		txt = fmt.Sprintf("\n\n    | %s", txt)
+	}
+	return fmt.Sprintf("at %s: %s%s", loc, msg, txt)
+}
+
+func (err CompilerError) Error() string {
+	return err.String()
+}
+
+func SortErrors(errs []error) {
+	sort.Slice(errs, func(i, j int) bool {
+		errA := errs[i]
+		errB := errs[j]
+		cmpErrA, okA := errA.(CompilerError)
+		cmpErrB, okB := errB.(CompilerError)
+		if okA != okB {
+			return !okA // non-compilation errors first
+		}
+
+		if orderByMessage := !okA; orderByMessage {
+			return cmp.Compare(errA.Error(), errB.Error()) < 0
+		}
+
+		return cmpErrA.Span.Compare(cmpErrB.Span) < 0
+	})
+}
