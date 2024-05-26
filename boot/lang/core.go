686a10e2444797610eeab4993498e8e0a793b3d1
ax-ab <98651633+ax-lab@users.noreply.github.com>
2024-05-26 22:20:58 +1200

    add lexer configuration to core

--- boot/lang/core.go
+++ boot/lang/core.go
@@ -4,11 +4,28 @@ import (
 	"axlab.dev/bit/core"
 )
 
-func Declare(comp *core.Compiler) error {
-	comp.DeclareOp(OpSegment)
-	comp.SetOutput(NoOp)
+func Declare(compiler *core.Compiler) error {
+	compiler.DeclareOp(OpSegment)
+	compiler.SetOutput(NoOp)
+
+	InitLexer(&compiler.Lexer)
+
 	return nil
 }
 
 func NoOp(list core.NodeList) {
+}
+
+func InitLexer(lexer *core.Lexer) {
+	lexer.AddBrackets("(", ")")
+	lexer.AddBrackets("[", "]")
+	lexer.AddBrackets("{", "}")
+	lexer.AddSymbols(
+		// punctuation
+		".", "..", ",", ";", ":",
+		// operators
+		"!", "?",
+		"=", "+", "-", "*", "/", "%",
+		"==", "!=", "<", "<=", ">", ">=",
+	)
 }
